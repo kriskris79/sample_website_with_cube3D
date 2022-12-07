4c533996b7b5e1c41bbe9dd5e4c1d295f5527b70
kriskris79 <krzysztofkakolewski@googlemail.com>
2022-12-07 11:51:36 +0000

    Footer_nr_2

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,13 +1,12 @@
 import React from "react";
-import {Carousel} from "react-bootstrap";
-
+import Carousel from 'react-bootstrap/Carousel';
 const Slider = () => {
     return (
 
 
 
             <Carousel>
-            <Carousel.Item interval={2000}>
+            <Carousel.Item interval={10000}>
             <img
             className="d-block w-100"
             src="https://images.pexels.com/photos/2253870/pexels-photo-2253870.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -18,7 +17,7 @@ const Slider = () => {
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item interval={2000}>
+            <Carousel.Item interval={10000}>
             <img
             className="d-block w-100"
             src="https://images.pexels.com/photos/313707/pexels-photo-313707.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -29,7 +28,7 @@ const Slider = () => {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Carousel.Caption>
             </Carousel.Item >
-                <Carousel.Item interval={2000}>
+                <Carousel.Item interval={10000}>
                     <img
                         className="d-block w-100"
                         src="https://images.pexels.com/photos/1045541/pexels-photo-1045541.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -42,7 +41,7 @@ const Slider = () => {
                         </p>
                     </Carousel.Caption>
                 </Carousel.Item>
-            <Carousel.Item interval={2000}>
+            <Carousel.Item interval={10000}>
             <img
             className="d-block w-100"
             src="https://images.pexels.com/photos/288008/pexels-photo-288008.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -55,7 +54,7 @@ const Slider = () => {
             </p>
             </Carousel.Caption>
             </Carousel.Item>
-                <Carousel.Item interval={2000}>
+                <Carousel.Item interval={10000}>
                     <img
                         className="d-block w-100"
                         src="https://images.pexels.com/photos/1345574/pexels-photo-1345574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -68,7 +67,7 @@ const Slider = () => {
                         </p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item interval={2000}>
+                <Carousel.Item interval={10000}>
                     <img
                         className="d-block w-100"
                         src="https://images.pexels.com/photos/1024968/pexels-photo-1024968.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -81,7 +80,7 @@ const Slider = () => {
                         </p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item interval={2000}>
+                <Carousel.Item interval={10000}>
                     <img
                         className="d-block w-100"
                         src="https://images.pexels.com/photos/1024968/pexels-photo-1024968.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
