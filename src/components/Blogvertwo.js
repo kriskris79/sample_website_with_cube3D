c79d8afb6b1b06affc7d1d376424015e7c1b2468
kriskris79 <krzysztofkakolewski@googlemail.com>
2022-12-10 01:48:49 +0000

    I_can't_see_my_posts

--- src/components/Blogvertwo.js
+++ src/components/Blogvertwo.js
@@ -1,7 +1,7 @@
 
 
 
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import Axios from 'axios'
 
 
@@ -10,16 +10,48 @@ import Axios from 'axios'
 
 function Blogvertwo() {
 
+    //hooks
+const [data, setDate] = useState([])
+// sample address of api https://jsonplaceholder.typicode.com/posts/
+
     useEffect(() => {
-        Axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(res => console.log(res.data)).catch(err => console.log(err))
+        Axios.get('https://krislens.com/wp-json/wp/v2/categories/14')
+            .then(res => {
+                console.log("Getting form data ::::", res.data)
+                setDate(res.data) //for hooks
+
+            }).catch(err => console.log('Error getting all data from API', err))
     }, [])
+
+
+    //for rendering
+    const arr = data.map((data, index) => {
+    return (
+
+        <tr>
+            <td>{data.id}</td>
+            <td>{data.name}</td>
+            <td>{data.link}</td>
+        </tr>
+
+    )
+    })
 
     return (
         <div className="blog-container">
-           <h1> test for using axios  </h1>
+           <h1> T  </h1>
 
+            <table>
+                <tbody>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Link</th>
+                </tr>
+                {arr}
 
+                </tbody>
+            </table>
 
         </div>
     );
