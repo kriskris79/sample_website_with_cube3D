2d456a4add967e2c4e1de7f5fd48d5b59358a5ac
kriskris79 <krzysztofkakolewski@googlemail.com>
2022-12-12 23:06:06 +0000

    new_pictures_in_local_folder

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,6 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Carousel } from 'react-bootstrap';
 import img1 from '../images/img1.jpg';
 import img2 from '../images/img2.jpg';
+import img3 from '../images/img3.gif';
+import img4 from '../images/img4.jpg';
+import img5 from '../images/img5.jpg';
+import img6 from '../images/img6.jpg';
+
 function Slider() {
 
     return (
@@ -34,7 +39,7 @@ function Slider() {
                     <Carousel.Item>
                         <img style={{maxHeight:"100vh"}}
                             className="d-block w-100"
-                             src="https://images.pexels.com/photos/1045541/pexels-photo-1045541.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                             src={img3}
                             alt="Third slide"
                         />
 
@@ -47,54 +52,45 @@ function Slider() {
                     <Carousel.Item>
                         <img style={{maxHeight:"100vh"}}
                              className="d-block w-100"
-                             src="https://images.pexels.com/photos/288008/pexels-photo-288008.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                             alt="Third slide"
+                             src={img4}
+                             alt="Forth slide"
                         />
 
                         <Carousel.Caption>
-                            <h3>Third Slide Image</h3>
-                            <p>Third Slide Description.</p>
+                            <h3>Forth Slide Image</h3>
+                            <p>Forth Slide Description.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
 
                     <Carousel.Item>
                         <img style={{maxHeight:"100vh"}}
                              className="d-block w-100"
-                             src="https://images.pexels.com/photos/1345574/pexels-photo-1345574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                             alt="Third slide"
+                             src={img5}
+                             alt="Fifth slide"
                         />
 
                         <Carousel.Caption>
-                            <h3>Third Slide Image</h3>
-                            <p>Third Slide Description.</p>
+                            <h3>Fifth Slide Image</h3>
+                            <p>Fifth Slide Description.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
 
                     <Carousel.Item>
                         <img style={{maxHeight:"100vh"}}
                              className="d-block w-100"
-                             src="https://images.pexels.com/photos/1024968/pexels-photo-1024968.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                             alt="Third slide"
+                             src={img6}
+                             alt="Sixth slide"
                         />
 
                         <Carousel.Caption>
-                            <h3>Third Slide Image</h3>
-                            <p>Third Slide Description.</p>
+                            <h3>Sixth Slide Image</h3>
+                            <p>Sixth Slide Description.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
 
-                    <Carousel.Item>
-                        <img style={{maxHeight:"100vh"}}
-                             className="d-block w-100"
-                             src="https://images.pexels.com/photos/1024968/pexels-photo-1024968.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                             alt="Third slide"
-                        />
 
-                        <Carousel.Caption>
-                            <h3>Third Slide Image</h3>
-                            <p>Third Slide Description.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
+
+
 
                 </Carousel>
             </div>
